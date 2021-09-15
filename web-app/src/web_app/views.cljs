cdcc23c2a6b47603f80ac0022f76940075115080
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-09-15 16:15:00 +0200

    WIP: Add web-app with mqtt-client

---
+++ web-app/src/web_app/views.cljs
@@ -0,0 +1,12 @@
+(ns web-app.views
+  (:require
+   [re-frame.core :as re-frame]
+   [web-app.subs :as subs]
+   ))
+
+(defn main-panel []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [:h1
+      "Hello from " @name]
+     ]))
