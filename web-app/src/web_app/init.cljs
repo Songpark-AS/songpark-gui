dd0436ad35786190889c79ec19753644d83c0653
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-09-16 15:47:38 +0200

    Use system-components, add 2 sliders that will be used to control the volume levels of the TPs, hook up buttons to: get TP status from platform, connect to mqtt and send a Hello World message

---
+++ web-app/src/web_app/init.cljs
@@ -0,0 +1,63 @@
+(ns web-app.init
+  (:require [com.stuartsierra.component :as component]
+            [re-frame.core :as rf]
+            [taoensso.timbre :as log]
+            ;; [songpark.common.taxonomy.teleporter]
+            ;; [songpark.common.taxonomy.mqtt]
+            [songpark.common.config :as config]
+            ;; [app.config :as app.config]
+            [web-app.communication :as communication]
+            [web-app.event :as event]
+            ;; [app.i18n :as i18n]
+            ;; [app.logging :as logging]
+            [web-app.mqtt :as mqtt]
+            ))
+
+(defonce -system (atom nil))
+
+
+(defrecord InitManager [started?]
+  component/Lifecycle
+  (start [this]
+    (if started?
+      this
+      (do (log/info "Starting InitManager")
+          (assoc this
+                 :started? true))))
+  (stop [this]
+    (if-not started?
+      this
+      (do (log/info "Stopping InitManager")
+          (assoc this
+                 :started? false)))))
+
+(defn init-manager [settings]
+  (map->InitManager settings))
+
+(defn- system-map [config-settings]
+  (let [#_#_config-manager (component/start (app.config/config-manager config-settings))]
+    (component/system-map
+     ;; :config-manager config-manager
+
+     :init          (component/using
+                     (init-manager (:init @config/config))
+                     [])
+     ;; :logging-manager (component/using
+     ;;                   (logging/logging-manager (:logging-manager @config/config))
+     ;;                   [])
+     :event-manager (component/using
+                     (event/event-manager (:event-manager @config/config))
+                     [])
+     ;; :i18n-manager (component/using
+     ;;                (i18n/i18n-manager (:i18n-manager @config/config))
+     ;;                [])
+     :communication-manager (component/using
+                             (communication/communication-manager (:communication-manager @config/config))
+                             [])
+     #_#_:mqtt-manager (component/using
+                    (mqtt/mqtt-manager (:mqtt-settings @config/config))
+                    []))))
+
+(defn init [config-settings]
+  (log/info "Initializing system")
+  (reset! -system (component/start (system-map config-settings))))
--- web-app/src/web_app/mqtt.cljs
+++ web-app/src/web_app/mqtt.cljs
@@ -2,6 +2,7 @@
   (:require [re-frame.core :as rf]
             [taoensso.timbre :as log]
             [songpark.common.protocols.mqtt :as protocol.mqtt]
+            [com.stuartsierra.component :as component]
             [songpark.common.config :as config]
             ["paho-mqtt" :as Paho]))
 
@@ -9,7 +10,6 @@
   (keyword (clojure.string/replace topic #"/" "_-_")))
 (defn keyword-to-topic [keyword]
   (clojure.string/replace (name keyword) #"_-_" "/"))
-
 
 (defn on-message-receive [message topic-handlers]
   (log/info ::MqttClient.receive "[MQTT rx]: " ^String(. message -payloadString))
@@ -20,38 +20,12 @@
 
 (defn on-connect [invocation-context mqtt-client]
   (log/debug ::MqttClient.on-connect "Connected to broker, " invocation-context)
-    (let [topic-handlers (:topic-handlers mqtt-client)
-          client (:client mqtt-client)]
-      (when (not (nil? @topic-handlers))
-        (log/debug ::MqttClient.on-connect "Resubscribing to topics")
+  (let [topic-handlers (:topic-handlers mqtt-client)
+        client (:client mqtt-client)]
+    (when (not (nil? @topic-handlers))
+      (log/debug ::MqttClient.on-connect "Resubscribing to topics")
       (doseq [[k f] @topic-handlers]
-        (do (protocol.mqtt/subscribe client (keyword-to-topic k) f))))
-    )
-  )
-
-#_(defn retry-connection [client]
-  (when (not (protocol.mqtt/is-connected? client))
-    (log/info ::retry-connection "Trying to reconnect to MQTT broker")
-    (-> (retry #(protocol.mqtt/connect client) 10 1000)
-        (.then #((log/debug ::retry-connection "Resubscribing to topics")
-                 (log/debug ::retry-connection "doseq thing: "
-                            (doseq [[k f] @(:topic-handlers client)]
-                              (do (protocol.mqtt/subscribe client (keyword-to-topic k) f))))))
-        (.catch js/Error #(log/error ::retry-connection %)))))
-
-#_(defn on-connection-lost [response-object client]
-  (log/info ::on-connection-lost "Lost connection to MQTT broker" (.-errorCode response-object) (.-errorMessage response-object))
-  (let [errorCode (.-errorCode response-object)]
-    (when (not (= errorCode 0))
-      (log/debug ::on-connection-lost "ErrorCode is not 0, we should try to reconnect")
-      (retry-connection client))))
-
-(defn- on-success [response]
-  (log/info ::on-success response))
-
-(defn- on-error [reject]
-  (log/error reject))
-
+        (do (protocol.mqtt/subscribe client (keyword-to-topic k) f))))))
 
 (defrecord MqttClient [config]
   protocol.mqtt/IMqtt
@@ -97,85 +71,85 @@
       (when (protocol.mqtt/is-connected? this)
         (.unsubscribe client topic)
         (log/debug ::MqttClient.unsubscribe "Unsubscribed from topic: " topic)
-        (swap! topic-handlers dissoc (topic-to-keyword topic))
-        ))
-    ))
-
-
+        (swap! topic-handlers dissoc (topic-to-keyword topic))))))
 
 (defn mqtt-client [config]
   (map->MqttClient
    (let [topic-handlers (atom {})]
-   {:config config
-    :topic-handlers topic-handlers
-    :client (let [{:keys [host port client-id]} config
-                  client (Paho/Client. host port client-id)]
-              (set! (.-onConnectionLost client) (fn [& args] (log/debug ::mqtt-client "Lost connection" args)))
-              (set! (.-onMessageArrived client) (fn [message] (on-message-receive message topic-handlers)))
-              #_(.on client "messageReceived" (fn [message] (on-message-receive message topic-handlers)))
-              client)})))
+     {:config config
+      :topic-handlers topic-handlers
+      :client (let [{:keys [host port client-id]} config
+                    client (Paho/Client. host port client-id)]
+                (set! (.-onConnectionLost client) (fn [& args] (log/debug ::mqtt-client "Lost connection" args)))
+                (set! (.-onMessageArrived client) (fn [message] (on-message-receive message topic-handlers)))
+                #_(.on client "messageReceived" (fn [message] (on-message-receive message topic-handlers)))
+                client)})))
 
+(defrecord MQTTManager [started? host port client-id]
+  component/Lifecycle
+  (start [this]
+    (if started?
+      this
+      (let [client (mqtt-client {:host host :port port :client-id client-id})
+            test-topic-handler (fn [message]
+                                 (let [payload ^String (. message -payloadString)]
+                                   (prn "Test topic handler")
+                                   (prn (str "payload: " payload))))]
+        (log/info "Starting MQTTManager")
+        (log/info "Connecting to broker")
+        (protocol.mqtt/connect client)
+        #_(protocol.mqtt/subscribe client "hello" test-topic-handler)
+        (assoc this :started? true :client client))))
+  (stop [this]
+    (if-not started?
+      this
+      (do (log/info "Stopping MQTTManager")
+          (let [client (:client this)]
+            (when (protocol.mqtt/is-connected? client)
+              (protocol.mqtt/disconnect client)))))))
 
+(defn mqtt-manager [settings]
+  (map->MQTTManager settings))
 
+(let [mqtt-settings (:mqtt-settings @config/config)]
+  (def client (mqtt-client mqtt-settings)))
 
+(defn connect []
+  (when (not (protocol.mqtt/is-connected? client))
+    (protocol.mqtt/connect client)))
+
+(defn publish! [topic message]
+  (protocol.mqtt/publish! client topic message))
 
 
 (comment
-  (:mqtt-settings @config/config)
-  retry
-  config
-  protocol.mqtt/connect
+
   (defn foo-handler [message]
     (let [payload ^String(. message -payloadString)]
       (prn "this is the footopic handler")
-      (prn (str "Here is the payload: " payload))
-      ))
+      (prn (str "Here is the payload: " payload))))
   (defn world-handler [message]
     (let [payload ^String(. message -payloadString)]
       (prn "this is the world handler")
-      (prn (str "Here is the payload: " payload))
-      ))
+      (prn (str "Here is the payload: " payload))))
   (defn my-handler [_]
     (prn "hello I am my-handler"))
 
   (let [mqtt-settings (:mqtt-settings @config/config)]
     (def client (mqtt-client mqtt-settings)))
 
-  (let [mqtt-settings {:host "127.0.0.1"
-                       :port 8000
-                       :client-id "this-should-be-auto-generated"}]
-    (def client (mqtt-client mqtt-settings)))
-  client
-  (count @(:topic-handlers client))
-  on-connection-lost
-  @(:connection-lost-event-listener client)
-  (.removeListener (:client client) "connectionLost" @(:connection-lost-event-listener client))
   (protocol.mqtt/is-connected? client)
-  (:client client)
   (protocol.mqtt/connect client)
-  (let [p-client (:client client)]
-    (.on p-client "connectionLost" (fn [response-object] (on-connection-lost response-object p-client))))
-  (let [p-client (:client client)
-        connection-lost-event-listener (:connection-lost-event-listener client)
-        listener-fn (fn [response-object] (on-connection-lost response-object p-client))]
-    (.on p-client "connectionLost" listener-fn)
-    (reset! connection-lost-event-listener listener-fn))
-    (-> (protocol.mqtt/connect client)
-      (.catch #(log/error "Muh err0r" %)))
-  (retry #(protocol.mqtt/connect client) 10 1000)
   (protocol.mqtt/disconnect client)
+
   (protocol.mqtt/subscribe client "world" world-handler)
   (protocol.mqtt/subscribe client "footopic" foo-handler)
   (protocol.mqtt/subscribe client "multi/level/topic" foo-handler)
-  (protocol.mqtt/unsubscribe client "multi/level/topic")
-  (protocol.mqtt/unsubscribe client "wild/foobar")
-  (protocol.mqtt/unsubscribe client "world")
-  (protocol.mqtt/send! client "World" "Hello")
 
-  (let [m (Message. "World")
-        _ (set! (.-destinationName m) "World")]
-    (pr-str (js->clj m))
-    ;;(send! mqc "World" m)
-    )
+  (protocol.mqtt/unsubscribe client "multi/level/topic")
+  (protocol.mqtt/unsubscribe client "footopic")
+  (protocol.mqtt/unsubscribe client "world")
+
+  (protocol.mqtt/publish! client "World" "Hello")
 
   )
