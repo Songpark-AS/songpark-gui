da91e70385da07650e04db74f4b087040bef0fef
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-09-27 14:53:50 +0200

    Display nickname of teleporter in teleporter.detail view

--- web-app/src/web_app/views/teleporter/detail.cljs
+++ web-app/src/web_app/views/teleporter/detail.cljs
@@ -1,14 +1,20 @@
 (ns web-app.views.teleporter.detail
   (:require
    [web-app.forms.ipv4 :as ipv4-form]
+   [taoensso.timbre :as log]
+   [re-frame.core :as rf]
    ))
 
 ;; Here be detailview of a teleporter
 ;; This view will contain configuration options for a teleporter
 
 
-(defn index []
-  [:div
-   [:h1 "Teleporter settings"]
-   [ipv4-form/ipv4-config "foo"]
-   ])
+(defn index [match]
+  (let [uuid (:id (:path-params match))
+        teleporters @(rf/subscribe [:teleporters])
+        nickname (->> teleporters (filter #(= (str (:uuid %)) uuid)) first :nickname)]
+    [:div.teleporter-detail-view
+     [:h1 "Teleporter settings"]
+     [:h2 nickname]
+     [ipv4-form/ipv4-config uuid]
+     ]))
