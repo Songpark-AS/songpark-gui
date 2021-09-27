6d2b097522e2b46d8b7ef58b02ddc698888965ea
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-09-27 14:45:32 +0200

    Workaround to get the ipv4 form to "ignore" the other fields when the "Use DHCP?" checkbox is checked

--- web-app/src/web_app/forms/ipv4.cljs
+++ web-app/src/web_app/forms/ipv4.cljs
@@ -3,13 +3,18 @@
    [re-frame.core :as rf]
    [reagent.core :as r]
    [ez-wire.form :as form]
+   [taoensso.timbre :as log]
    [ez-wire.form.helpers :refer [valid?]]
-   ["@material-ui/core/Button$default" :as Button]
-   ["@mui/material/TextField$default" :as TextField])
+   ["antd" :refer [Button Input Checkbox Alert]]
+   )
   (:require-macros [ez-wire.form.macros :refer [defform]]
                    [ez-wire.form.validation :refer [defvalidation]]))
 
-(defvalidation :teleporter/ip "Invalid IP-address")
+(defvalidation :teleporter/ip (fn [_] [:> Alert {:type "error"
+                                                 :message "Invalid IP-address"
+                                                 :showIcon true}]))
+
+
 
 (defn text-adapter [{:keys [element] :as field}]
   (let [f (r/adapt-react-class element)]
@@ -25,7 +30,7 @@
 (defn checkbox-adapter [{:keys [element] :as field}]
   (let [f (r/adapt-react-class element)]
     (fn [{:keys [model] :as data}]
-      [f (merge {:value @model
+      [f (merge {:checked @model
                  :on-change #(reset! model (-> % .-target .-checked))}
                 (select-keys data [:id]))])))
 
@@ -55,34 +60,38 @@
                                    {:fields {:ip/address {:disabled false}
                                              :ip/subnet {:disabled false}
                                              :ip/gateway {:disabled false}}}))}}
-  [{:element TextField
+  [{:element Input
     :adapter text-adapter
     :placeholder "192.168.0.123"
     :label "IP address"
     :name :ip/address
     :validation :teleporter/ip}
-   {:element TextField
+   {:element Input
     :adapter text-adapter
     :placeholder "255.255.255.0"
     :label "Subnet mask"
     :name :ip/subnet
     :validation :teleporter/ip}
-   {:element TextField
+   {:element Input
     :adapter text-adapter
     :placeholder "192.168.0.1"
     :label "Gateway address"
     :name :ip/gateway
     :validation :teleporter/ip}
-   {:element input-checkbox
+   {:element Checkbox
+    :adapter checkbox-adapter
     :label "Use DHCP?:"
     :name :ip/dhcp?}])
 
 (defn ipv4-config [tp-id]
   (let [form (form-ipv4 {} nil)
         data-form (rf/subscribe [:ez-wire.form/on-valid (:id form)])]
-    (fn []
+    (fn [tp-id]
       [:div
+       [:pre (pr-str @data-form)]
+       [:pre (pr-str @(:data form))]
        [:h4 "Network settings"]
        [form/as-table {} form]
-       [:> Button {:color "primary" :variant "contained" :disabled (not (valid? data-form))} "Save"]
+       [:> Button {:type "primary" :disabled (not (or (:ip/dhcp? @(:data form))
+                                                      (valid? data-form)))} "Save"]
        ])))
