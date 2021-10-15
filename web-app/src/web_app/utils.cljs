9cca84ca26b2c1828146fa6ae4d9b20385e38430
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-10-15 17:10:08 +0200

    Change structure of :selected-teleporters to map with uuid as key, messagehandlers for sending volume/balance values to tpx

--- web-app/src/web_app/utils.cljs
+++ web-app/src/web_app/utils.cljs
@@ -22,3 +22,23 @@
   ;; this is considered bad practice. If we can find
   ;; another way to do it, we should consider it
   (js* "'ontouchstart' in window"))
+
+(defn scale-value
+  "Linearly transforms x from range input-range to output-range where:
+
+   input-range - a vector like [min max]
+   output-range - a vector like [min max]
+
+   "
+  [x input-range output-range]
+  (let [[a b] input-range
+        [c d] output-range]
+    (+
+     (-> (- x a)
+         (/ (- b a))
+         - ; negate the result
+         inc
+         (* c))
+     (-> (- x a)
+         (/ (- b a))
+         (* d)))))
