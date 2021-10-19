1bb3b1e72fb53b8d369f56703917db6be97af112
Emil Bengtsson <emil@emil0r.com>
2021-10-20 00:17:06 +0200

    Update jam subscriptions
    Now correctly connects with the jam.

--- web-app/src/web_app/message/dispatch/app.cljs
+++ web-app/src/web_app/message/dispatch/app.cljs
@@ -1,15 +1,22 @@
 (ns web-app.message.dispatch.app
   (:require [taoensso.timbre :as log]
             [songpark.common.protocol.mqtt.manager :as protocol.mqtt.manager]
+            [web-app.data :as data]
             [web-app.message.dispatch.interface :as message]))
 
 
-(defmethod message/dispatch :app.cmd/subscribe [{:message/keys [topics]
-                                         :keys [mqtt-manager]}]
-  (log/debug ::subscribe (str "Subscribing to " topics))
-  (protocol.mqtt.manager/subscribe mqtt-manager topics))
+;; topic is jam-id
+(defmethod message/dispatch :app.cmd/subscribe [{:message/keys [topic]
+                                                 :keys [mqtt-manager]}]
+  (data/set-jam-id! topic)
+  (let [topic (data/get-jam)]
+    (log/debug ::subscribe (str "Subscribing to " topic))
+    (protocol.mqtt.manager/subscribe mqtt-manager [topic])))
 
-(defmethod message/dispatch :app.cmd/unsubscribe [{:message/keys [topics]
+(defmethod message/dispatch :app.cmd/unsubscribe [{:message/keys [topic]
                                                    :keys [mqtt-manager]}]
-  (log/debug ::unsubscribe (str "Unsubscribing: " topics))
-  (protocol.mqtt.manager/unsubscribe mqtt-manager topics))
+  (let [topic (data/get-jam)]
+    (log/debug ::unsubscribe (str "Unsubscribing: " topic))
+    (protocol.mqtt.manager/unsubscribe mqtt-manager [topic])
+    (data/clear-jam-id!)))
+
