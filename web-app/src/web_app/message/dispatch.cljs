d62079ee6eb95b24dca43f9d9fa56529e33cbb43
Emil Bengtsson <emil@emil0r.com>
2021-10-21 23:28:05 +0200

    Fix styling issues
    Wrong alphabetical order.

--- web-app/src/web_app/message/dispatch.cljs
+++ web-app/src/web_app/message/dispatch.cljs
@@ -1,9 +1,9 @@
 (ns web-app.message.dispatch
-  (:require [web-app.message.dispatch.teleporter]
-            [web-app.message.dispatch.platform]
+  (:require [web-app.message.dispatch.app]
             [web-app.message.dispatch.jam]
-            [web-app.message.dispatch.app]
-            [web-app.message.dispatch.interface :as interface]))
+            [web-app.message.dispatch.interface :as interface]
+            [web-app.message.dispatch.platform]
+            [web-app.message.dispatch.teleporter]))
 
 (defn handler [msg]
   (interface/dispatch msg))
