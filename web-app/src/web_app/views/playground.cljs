375f18a9ebed7394e40ce9c9fc1c83cb0b0527cf
Emil Bengtsson <emil@emil0r.com>
2021-10-22 22:53:18 +0200

    Remove playground
    Not surving any purpose

--- web-app/src/web_app/views/playground.cljs
+++
@@ -1,7 +0,0 @@
-(ns web-app.views.playground
-  (:require ["antd" :refer [Button]]))
-
-(defn index []
-  [:div.playground
-   [:p "here be playground"]
-   [:> Button {:type "primary"} "I am button"]])
