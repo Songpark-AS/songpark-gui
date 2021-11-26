548ed4200ce8566855e823bef69afc603547118d
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-11-26 15:01:35 +0100

    Add namespace and hostname variables to songpark-webapp.template.yaml

--- deployment/k8s.prep.sh
+++ deployment/k8s.prep.sh
@@ -5,10 +5,14 @@ TEMPLATE_FILE=songpark-webapp.template.yaml
 YAML_FILE=songpark-webapp.yaml
 TAG=$1
 VERSION=$2
+NAMESPACE=songpark-$TAG
+HOSTNAME=songpark-$TAG.inonit.no
 
 echo "Copying template file"
 cp $DEPLOYMENT_DIR/$TEMPLATE_FILE $DEPLOYMENT_DIR/$YAML_FILE
 
 echo "Updating variables"
-sed -i '' 's/VAR__VERSION/'"$VERSION"'/g' $DEPLOYMENT_DIR/$YAML_FILE
-sed -i '' 's/VAR__TAG/'"$TAG"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__VERSION/'"$VERSION"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__TAG/'"$TAG"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__NAMESPACE/'"$NAMESPACE"'/g' $DEPLOYMENT_DIR/$YAML_FILE
+sed -i -e 's/VAR__HOSTNAME/'"$HOSTNAME"'/g' $DEPLOYMENT_DIR/$YAML_FILE
