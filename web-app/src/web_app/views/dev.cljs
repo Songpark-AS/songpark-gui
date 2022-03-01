93e3aaf065dec347e1222e5ad62d2e21d071cd10
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2022-03-01 16:00:28 +0100

    Remove uneven space on top and bottom of cards that appeared when using grid in the cards, Add prop to make the slider red when input is overloaded, WIP playout delay info display, misc layout changes in slider, misc color changes.

--- web-app/src/web_app/views/dev.cljs
+++ web-app/src/web_app/views/dev.cljs
@@ -6,29 +6,61 @@
             [taoensso.timbre :as log]))
 
 
-(defn slider [label sliderprops]
-  [:div.sp-slider
-   [:span.label label]
-   [:> Slider sliderprops]])
+(defn slider [label props]
+  (let [{:keys [overloading? mode pd-measured]} props]
+    (fn [label props]
+      [:div {:class (if overloading? "sp-slider overload" "sp-slider")}
+       [:span.label label]
+       [:div.sp-slider-clip
+        [:> Slider props]]
+       (when (= mode "playoutdelay")
+         [:div.pd-display
+          [:span.pd-value "12ms"]
+          (when pd-measured
+            [:span.pd-measured
+             [:span.pd-measured-triangle.triangle-up]
+             [:span.pd-measured-text (str "Measured: " pd-measured "ms")]])])])))
+
+(defn teleporter-switcher [tps]
+  (let [swipe-state (r/atom {:width 0 :start-x 0 :end-x 0 :direction nil})]
+    [:div.tps
+     {:on-touch-start (fn [e] (reset! swipe-state {:width (.. e -target -offsetWidth)
+                                                  :start-x (aget e "touches" 0 "pageX")
+                                                  :end-x (aget e "touches" 0 "pageX")}))
+      :on-touch-move (fn [e] (swap! swipe-state assoc :end-x (aget e "touches" 0 "pageX"))
+                      (let [{:keys [width start-x end-x]} @swipe-state
+                            direction (if (> end-x start-x) "right" "left")]
+                        (swap! swipe-state assoc :direction direction)
+                        (log/debug :on-touch-end "start" start-x "end" end-x)
+                        ))
+      :on-touch-end (fn [_]
+                      (let [direction (:direction @swipe-state)]
+                        (log/debug :on-touch-end direction)))}
+     (for [tp tps]
+       [:> Button {:key (:teleporter/uuid tp) :type "primary" :shape "round" :size "large"} (:teleporter/nickname tp)])
+     ])
+  )
 
 (defn index []
   [:div.dev
-   [:div.tp-scroller
-    [:div.tps
-     [:> Button {:type "primary" :shape "round" :size "large"} "MADONNA"]
-     [:> Button {:type "primary" :shape "round" :size "large"} "JIMI HENDRIX"]
-     [:> Button {:type "primary" :shape "round" :size "large"} "ELVIS PRESLEY"]
-     [:> Button {:type "primary" :shape "round" :size "large"} "ADELE"]]
-    [:p.status "Not in jam"]]
-   [:> Card [:<>
+   [teleporter-switcher [{:teleporter/uuid "tid1" :teleporter/nickname "TP1"}
+                         {:teleporter/uuid "tid2" :teleporter/nickname "TP2"}
+                         {:teleporter/uuid "tid3" :teleporter/nickname "TP3"}
+                         {:teleporter/uuid "tid4" :teleporter/nickname "TP4"}
+                         {:teleporter/uuid "tid5" :teleporter/nickname "TP5"}
+                         {:teleporter/uuid "tid6" :teleporter/nickname "TP6"}]]
+   ;; [teleporter-switcher (into [] (vals @(rf/subscribe [:teleporters])))]
+   ;; [:div.tp-scroller
+   ;;  [:p.status "Not in jam"]]
+   [:> Card {:bordered false} [:<>
              [slider "MASTER" {:tooltipVisible false :defaultValue 50}]
              [slider "LOCAL" {:tooltipVisible false :defaultValue 50}]
              [slider "NETWORK" {:tooltipVisible false :defaultValue 50}]
              ]]
-   [:> Card [:<>
-             [slider "PLAYOUT DELAY" {:tooltipVisible false :defaultValue 65}]
+   [:> Card {:bordered false} [:<>
+             [slider "PLAYOUT DELAY" {:tooltipVisible false :defaultValue 65 :mode "playoutdelay" :pd-measured 6}]
              ]]
-   [:> Card [:<>
+   [:> Card {:bordered false} [:<>
              [:> Radio.Group {:defaultValue "stereo" :buttonStyle "solid"}
               [:> Radio.Button {:value "mono"} "Mono"]
               [:> Radio.Button {:value "stereo"} "Stereo"]]
@@ -37,11 +69,11 @@
              [:> Radio.Group {:defaultValue "line" :buttonStyle "solid"}
               [:> Radio.Button {:value "instrument"} "Instrument"]
               [:> Radio.Button {:value "line"} "Line"]]
-             [slider "GAIN" {:tooltipVisible false :defaultValue 50}]
+             [slider "GAIN" {:tooltipVisible false :defaultValue 80}]
              [:> Divider {:orientation "right"} "RIGHT"]
              [:> Radio.Group {:defaultValue "line" :buttonStyle "solid"}
               [:> Radio.Button {:value "instrument"} "Instrument"]
               [:> Radio.Button {:value "line"} "Line"]]
-             [slider "GAIN" {:tooltipVisible false :defaultValue 80}]
+             [slider "GAIN" {:tooltipVisible false :defaultValue 80 :overloading? true}]
              ]]
    ])
