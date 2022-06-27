994b748f979f92a8c32c6705dc9ac6e55b35d756
Emil Bengtsson <emil@emil0r.com>
2022-06-27 15:32:28 +0200

    Add message-base to mqtt re-frame fx
    Refactor mqtt re-frame fxs to include :teleporter/id and :auth.user/id
    in their messages, to go along with the pairing.

--- web-app/src/web_app/event/mqtt.cljs
+++ web-app/src/web_app/event/mqtt.cljs
@@ -3,6 +3,7 @@
             [songpark.mqtt.util :refer [teleporter-topic]]
             [re-frame.core :as rf]
             [taoensso.timbre :as log]
+            [web-app.event.util :refer [message-base]]
             [web-app.mqtt.interceptor :refer [mqtt-client]]))
 
 (rf/reg-event-fx
@@ -34,13 +35,21 @@
 (rf/reg-event-fx
  :mqtt/publish-message
  [mqtt-client]
- (fn [{:keys [mqtt-client]} [_ topic message]]
-   (mqtt/publish mqtt-client topic message)
+ (fn [{:keys [mqtt-client db]} [_ topic message]]
+   (mqtt/publish mqtt-client
+                 topic
+                 (message-base
+                  db
+                  message))
    nil))
 
 (rf/reg-event-fx
  :mqtt/send-message-to-teleporter
  [mqtt-client]
- (fn [{:keys [mqtt-client]} [_ teleporter-id message]]
-   (mqtt/publish mqtt-client (teleporter-topic teleporter-id) message)
+ (fn [{:keys [mqtt-client db]} [_ teleporter-id message]]
+   (mqtt/publish mqtt-client
+                 (teleporter-topic teleporter-id)
+                 (message-base
+                  db
+                  message))
    nil))
