245e6cf435b13b2bad3be11eced1b2f2798e8f0b
Emil Bengtsson <emil@emil0r.com>
2022-06-30 16:22:11 +0200

    Styled topbar
    - Reorganized styling
    - Minor changes to topbar in order to support the styling
    - Added color palette
    - Removed unused styling

---
+++ web-app/src/web_app/components/icon.cljs
@@ -0,0 +1,12 @@
+(ns web-app.components.icon)
+
+(defn room []
+  [:span.material-symbols-outlined "language"])
+(defn levels []
+  [:span.material-symbols-outlined "tune"])
+(defn input []
+  [:span.material-symbols-outlined "input"])
+(defn backspace []
+  [:span.material-symbols-outlined "backspace"])
+(defn account []
+  [:span.material-symbols-outlined "account_circle"])
--- web-app/src/web_app/views/topbar.cljs
+++ web-app/src/web_app/views/topbar.cljs
@@ -1,25 +1,31 @@
 (ns web-app.views.topbar
   (:require ["antd" :refer [Button]]
+            [clojure.string :as str]
             [re-frame.core :as rf]
             [reagent.core :as r]
-            [reitit.frontend.easy :as rfe]))
+            [reitit.frontend.easy :as rfe]
+            [web-app.components.icon :as icon]))
 
 
 (defn- show-profile [profile]
   (let [{:profile/keys [name position image-url]} @profile]
     [:div.profile
      {:on-click #(rfe/push-state :views/profile)}
-     [:img {:src image-url}]
-     [:span.name name]
-     [:span.position position]]))
+     [:div.account
+      (if-not (str/blank? image-url)
+        [:img {:src image-url}]
+        [icon/account])]
+     [:div.details
+      [:span.name name]
+      [:span.position position]]]))
 
 (defn- show-teleporter-status [teleporter]
   (r/with-let [paired? (rf/subscribe [:teleporter/paired?])]
     [:div.status
      {:on-click #(rfe/push-state :views/teleporter)}
      (if @paired?
-       "Linked"
-       "Not linked")]))
+       [:span.linked "Linked"]
+       [:span.not-linked "Not linked"])]))
 
 (defn index []
   (r/with-let [profile (rf/subscribe [:profile/profile])]
