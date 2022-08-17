bab0aaa8dfe408dfaed8e7021c2179fdc0c2aa61
Emil Bengtsson <emil@emil0r.com>
2022-08-17 09:03:23 +0200

    Style teleporter
    Looks better squeezed. Still have to look at how it works with a
    connected Teleporter.

--- web-app/src/web_app/views/teleporter.cljs
+++ web-app/src/web_app/views/teleporter.cljs
@@ -3,14 +3,22 @@
             [re-frame.core :as rf]
             [reagent.core :as r]
             [reitit.frontend.easy :as rfe]
+            [web-app.history :refer [back]]
             [web-app.views.teleporter.details :as teleporter.details]))
 
 (defn index []
   (r/with-let [paired? (rf/subscribe [:teleporter/paired?])]
     [:div.teleporter
      (if-not @paired?
-       [:> Button
-        {:type "primary"
-         :on-click #(rfe/push-state :views.teleporter/pair)}
-        "You are not paired. Pair a teleporter?"]
+       [:div.teleporter.squeeze
+        [:div.intro
+         [:div.title "You are not linked"]
+         [:div.slogan "Link a teleporter?"]]
+        [:div.back
+         {:on-click #(back)}
+         "Go back"]
+        [:> Button
+         {:type "primary"
+          :on-click #(rfe/push-state :views.teleporter/pair)}
+         "Link"]]
        [teleporter.details/index])]))
