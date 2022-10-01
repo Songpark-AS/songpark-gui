06f044507e9ac8e2fd9d2db4b317b8b542c2edde
Emil Bengtsson <emil@emil0r.com>
2022-10-01 22:47:44 +0200

    Fix days passed since last played something

--- web-app/src/web_app/views/room_host.cljs
+++ web-app/src/web_app/views/room_host.cljs
@@ -6,11 +6,44 @@
             [tick.core :as t]
             [web-app.components.icon :refer [add]]))
 
+
+(defn leap-year? [year]
+  (cond (and (zero? (mod year 4))
+             (or (zero? (mod year 400))
+                 (not (zero? (mod year 100)))))
+        true
+        :else
+        false))
+
+(def days-passed-per-month
+  {1 31
+   3 (+ 31 28 31)
+   4 (+ 30 31 28 31)
+   5 (+ 31 30 31 28 31)
+   6 (+ 30 31 30 31 28 31)
+   7 (+ 31 30 31 30 31 28 31)
+   8 (+ 31 31 30 31 30 31 28 31)
+   9 (+ 30 31 31 30 31 30 31 28 31)
+   10 (+ 31 30 31 31 30 31 30 31 28 31)
+   11 (+ 30 31 30 31 31 30 31 30 31 28 31)
+   12 (+ 31 30 31 30 31 31 30 31 30 31 28 31)})
+
+(defn get-month-days [leap? number-month]
+  (cond (and leap?
+             (= number-month 2))
+        (+ 31 29)
+
+        (= number-month 2)
+        (+ 31 28)
+        :else
+        (get days-passed-per-month number-month)))
+
 (defn get-compareable-days [inst]
-  (let [inst-year (t/int (t/year inst))
-        inst-month (t/int (t/month inst))
-        inst-day (t/day-of-month inst)]
-    (+ inst-year inst-month inst-day)))
+  (let [number-year (t/int (t/year inst))
+        days-year (* 365 number-year)
+        days-month (get-month-days (leap-year? number-year) (t/int (t/month inst)))
+        days-day (t/day-of-month inst)]
+    (+ days-year days-month days-day)))
 
 (defn get-time-display [days-today last-jammed]
   (let [days-jammed (get-compareable-days last-jammed)]
